4274360344064b449975acdb35be7f9fa056eff7
123mahmud <37867983+123mahmud@users.noreply.github.com>
2019-02-25 09:14:31 +0700

    86

--- app/Modules/Purchase/views/rencanabahanbaku/index.blade.php
+++ app/Modules/Purchase/views/rencanabahanbaku/index.blade.php
@@ -49,11 +49,10 @@
 
           <ul id="generalTab" class="nav nav-tabs">
             <li class="active"><a href="#alert-tab" data-toggle="tab">Rencana Bahan Baku Produksi</a></li>
+             <li><a href="#note-tab" data-toggle="tab">Relasi Barang Supplier</a>
           </ul>
 
           <div id="generalTabContent" class="tab-content responsive">
-
-            {!!$tambah!!}
             <div id="alert-tab" class="tab-pane fade in active">
               <div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12">
@@ -121,7 +120,59 @@
 
             <div id="note-tab" class="tab-pane fade">
               <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12"></div>
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                  <div class="row" style="margin-top:-20px;">
+                  <div class="col-md-12 col-sm-12 col-xs-12">
+                    <form id="ItemHarga">
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">Pilih Supplier :</label>
+                      </div>
+
+                      <div class="col-md-3 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <select class="form-control input-sm select2" name="supplier" id="idSupp" onchange="pilihSupplier()" >
+                            @foreach ($supplier as $data)
+                              <option value="{{ $data->s_id }}">{{ $data->s_company}}</option>
+                            @endforeach
+                          </select>
+                        </div>
+                      </div>
+
+                      <div class="col-md-12">
+                        <div class="col-md-12 tamma-bg"  style="margin-top: 5px;margin-bottom: 5px;
+                        margin-bottom: 40px; padding-bottom:20px;padding-top:20px;">
+                          <div class="col-md-12">
+                              <label class="control-label tebal" for="">Masukan Kode / Nama</label>
+                              <div class="input-group input-group-sm" style="width: 100%;">
+                                 <input type="text" class="form-control input-sm" id="cariItem" name="">
+                                 <input type="hidden" class="form-control input-sm" id="i_id" name="i_id">
+                              </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="panel-body">
+                      <div class="table-responsive">
+                        <table class="table tabelan table-hover table-responsive table-bordered" width="100%" cellspacing="0" id="tbl_groupitem">
+                          <thead>
+                            <tr>
+                              <th class="wd-15p">Kode - Nama Item</th>
+                              <th class="wd-15p">Aksi</th>
+                            </tr>
+                          </thead>
+
+                          <tbody>
+
+                          </tbody>
+
+
+                        </table>
+                      </div>
+                    </div>
+                  </form>
+                  </div>
+
+                </div>
+                </div>
               </div>
             </div>
 
@@ -130,6 +181,7 @@
                 <div class="panel-body"></div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
@@ -137,9 +189,6 @@
   </div>
 </div>
 <!--END PAGE WRAPPER-->
-<!-- modal-detail -->
-
-{!!$modalDetail!!}
 @endsection
 @section("extra_scripts")
 <script src="{{ asset ('assets/script/icheck.min.js') }}"></script>
@@ -158,6 +207,23 @@
     $('input.numberinput').bind('keypress', function (e) {
       return (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57) && e.which != 46) ? false : true;
     });
+
+    $('.select2').select2();
+
+    function __debounce(calback, timer){
+        window.clearTimeout(windowTimeOut);
+
+        windowTimeOut = window.setTimeout(calback, timer)
+    }
+
+    $( "#cariItem" ).autocomplete({
+          source: baseUrl+'/purchasing/get-item/autocomplete',
+          minLength: 1,
+          select: function(event, ui) {
+            $('#i_id').val(ui.item.id);
+            }
+        });
+
 
     var date = new Date();
     var newdate = new Date(date);
@@ -326,8 +392,154 @@
     return text;
   }
 
-  function refreshTabel() {
-    $('#data').DataTable().ajax.reload();
-  }
+   function refreshTabel() {
+      $('#data').DataTable().ajax.reload();
+   }
+
+   pilihSupplier();
+
+   function pilihSupplier(){
+     var x = document.getElementById("idSupp").value;
+     $('#tbl_groupitem').dataTable().fnDestroy();
+     $('#tbl_groupitem').DataTable({
+         processing: true,
+         // responsive:true,
+         serverSide: true,
+         ajax: {
+             url: baseUrl + '/master/supplier/table/'+x,
+         },
+         "columns": [
+         { "data": "i_name", width: '90%' },
+         { "data": "action", width: '10%' },
+         ],
+         "responsive":true,
+
+               "pageLength": 10,
+             "lengthMenu": [[10, 20, 50, - 1], [10, 20, 50, "All"]],
+             "language": {
+                 "searchPlaceholder": "Cari Data",
+                 "emptyTable": "Tidak ada data",
+                 "sInfo": "Menampilkan _START_ - _END_ Dari _TOTAL_ Data",
+                 "sSearch": '<i class="fa fa-search"></i>',
+                 "sLengthMenu": "Menampilkan &nbsp; _MENU_ &nbsp; Data",
+                 "infoEmpty": "",
+                 "paginate": {
+                         "previous": "Sebelumnya",
+                         "next": "Selanjutnya",
+                      }
+               }
+     });
+   }
+
+   $('#cariItem').keypress(function(e)
+   {
+      var charCode;
+      if ((e.which && e.which == 13)) 
+      {
+         charCode = e.which;
+      }
+      else if (window.event) 
+      {
+         e = window.event;
+         charCode = e.keyCode;
+      }
+      if ((e.which && e.which == 13))
+      {
+         var bahan_baku  = $('#cariItem').val();
+         var qty         = $('#i_id').val();
+         if(bahan_baku == '' || qty == '')
+         {
+            toastr.warning('Item Harus di Isi');
+            return false;
+         }
+         else
+         {
+            tambahItemSupp();
+            $('#cariItem').val('');
+            $('#i_id').val('');
+            return false;
+         }
+
+      }
+   });
+
+   function tambahItemSupp(){
+      $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+          });
+        $('.submit-group').attr('disabled', 'disabled');
+        $.ajax({
+            url: baseUrl + "/master/supplier/tambahSupp",
+            type: 'GET',
+            data: $('#ItemHarga').serialize(),
+            success: function (response) {
+                if (response.status == 'sukses') {
+                     pilihSupplier();
+                     $('#cariItem').val('');
+                     $('#i_id').val('');
+                    iziToast.success({
+                        timeout: 5000,
+                        position: "topRight",
+                        icon: 'fa fa-chrome',
+                        title: '',
+                        message: 'Data Berhasil di Tambah.'
+                    });
+                } else {
+                    iziToast.error({
+                        position: "topRight",
+                        title: '',
+                        message: 'Data Gagal Menambah.'
+                    });
+                    $('.submit-group').removeAttr('disabled', 'disabled');
+                }
+            }
+        })
+    }
+
+   function hapus(a) {
+        iziToast.show({
+          color: 'red',
+          title: 'Peringatan',
+          message: 'Apakah anda yakin!',
+          position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter
+          progressBarColor: 'rgb(0, 255, 184)',
+          buttons: [
+            [
+              '<button>Ok</button>',
+              function (instance, toast) {
+                instance.hide({
+                  transitionOut: 'fadeOutUp'
+                }, toast);
+                var idGroup = $('#idGroup').val();
+                console.log(idGroup);
+                $.ajax({
+                     type: "get",
+                     url: baseUrl + '/master/supplier/hapus/'+a,
+                     data: {idGroup},
+                     success: function(response){
+                          if (response.status=='sukses') {
+                            toastr.info('Data berhasil di hapus.');
+                            pilihSupplier();
+                          }else{
+                            toastr.error('Data gagal di simpan.');
+                          }
+                        }
+                     })
+              }
+            ],
+            [
+              '<button>Close</button>',
+               function (instance, toast) {
+                instance.hide({
+                  transitionOut: 'fadeOutUp'
+                }, toast);
+              }
+            ]
+          ]
+        });
+
+      }
 </script>
 @endsection()
