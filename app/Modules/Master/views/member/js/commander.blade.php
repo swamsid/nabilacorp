d8671ee66826b5e6c558b6dc77c6fbb10318db0f
dirga <swamsid@gmail.com>
2019-03-11 09:12:14 +0700

    dirga sampler

--- app/Modules/Master/views/member/js/commander.blade.php
+++ app/Modules/Master/views/member/js/commander.blade.php
@@ -17,10 +17,9 @@
 				{
 					data : null,
 					render : function(data) {
-						var preview_btn = '<button onclick="open_preview(' + data.c_id + ')" class="btn btn-info btn-sm" title="edit" style=""><i class="fa fa-eye"></i></button>';
-						var edit_btn = '<button onclick="open_form_alter(' + data.c_id + ')" class="btn btn-primary btn-sm" title="edit" style=""><i class="fa fa-pencil"></i></button>';
-						var remove_btn = '<button onclick="remove_data(' + data.c_id + ')" class="btn btn-danger btn-sm" title="edit"><i class="fa fa-trash-o"></i></button>';
-						var buttons = '<div class="btn-group">' + preview_btn + edit_btn + remove_btn + "</div>";
+						var edit_btn = '<button onclick="open_form_alter(' + data.c_id + ')" class="btn btn-warning btn-xs" title="edit" style=""><i class="fa fa-pencil"></i></button>';
+						var remove_btn = '<button onclick="remove_data(' + data.c_id + ')" class="btn btn-danger btn-xs" title="edit"><i class="fa fa-trash-o"></i></button>';
+						var buttons = '<div class="btn-group">' + edit_btn + remove_btn + "</div>";
 
 						return buttons;
 					}
